bd655a273e678ee45c835f571ba6b73fa309cd70
赖尧 <yujitang_2006@qq.com>
2025-10-31 16:13:51 +0800

    增加历史记录模块，优化web端ui界面

---
+++ web/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8080', // 后端服务
+        changeOrigin: true,
+      },
+      '/shorten': {
+        target: 'http://127.0.0.1:8080', // 后端服务
+        changeOrigin: true,
+      }
+    }
+  }
+})
