1204da831f32c74def10b99a72ac844ea07ce8f2
赖尧 <yujitang_2006@qq.com>
2025-11-08 23:44:44 +0800

    新增readme文档

--- src/main/java/com/layor/tinyflow/TinyFlowApplication.java
+++ src/main/java/com/layor/tinyflow/TinyFlowApplication.java
@@ -2,8 +2,17 @@ package com.layor.tinyflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 @EnableAsync
@@ -13,5 +22,18 @@ public class TinyFlowApplication {
     public static void main(String[] args) {
         SpringApplication.run(TinyFlowApplication.class, args);
     }
+    @Service
+    public static class LogService {
+        @Async("taskExecutor")
+        public void recordVisit(String code) {
+        }
+    }
 
+        @Bean("taskExecutor")
+        public Executor taskExecutor() {
+            ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+            //配置参数...省略
+            executor.initialize();
+            return executor;
+        }
 }
